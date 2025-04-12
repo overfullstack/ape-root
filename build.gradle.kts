e0ec9d7d9f4ffb790d35889987c91470d00c023d
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2025-04-12 16:03:49 +0530

    Initial commit
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+/**
+ * ************************************************************************************************
+ * Copyright (c) 2023, Salesforce, Inc. All rights reserved. SPDX-License-Identifier: Apache License
+ * Version 2.0 For full license text, see the LICENSE file in the repo root or
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * ************************************************************************************************
+ */
+plugins {
+  id("ape.root-conventions")
+  id("ape.publishing-conventions")
+  id("ape.kt-conventions")
+  alias(libs.plugins.moshix)
+  alias(libs.plugins.node.gradle)
+  alias(libs.plugins.kover)
+  alias(libs.plugins.nexus.publish)
+  alias(libs.plugins.gradle.taskinfo)
+}
+
+val mockitoAgent = configurations.create("mockitoAgent")
+
+dependencies {
+  api(platform(libs.http4k.bom))
+  api(libs.revoman)
+  api(libs.bundles.http4k)
+  api(libs.moshix.adapters)
+  api(libs.java.vavr)
+  api(libs.kotlin.vavr)
+  api(libs.arrow.core)
+  api(libs.kotlinx.datetime)
+  implementation(libs.bundles.kotlin.logging)
+  implementation(libs.pprint)
+  implementation(libs.graal.js)
+  implementation(libs.kotlin.faker)
+  implementation(libs.underscore)
+  implementation(libs.okio.jvm)
+  implementation(libs.spring.beans)
+  kapt(libs.immutables.value)
+  compileOnly(libs.immutables.builder)
+  compileOnly(libs.immutables.value.annotations)
+  compileOnly(libs.jetbrains.annotations)
+  testImplementation(libs.truth)
+  testImplementation(libs.json.assert)
+  mockitoAgent(libs.mockito.core) { isTransitive = false }
+  testImplementation(libs.mockk)
+}
+
+testing {
+  suites {
+    val test by getting(JvmTestSuite::class) { useJUnitJupiter(libs.versions.junit.get()) }
+
+    register<JvmTestSuite>("integrationTest") {
+      dependencies {
+        implementation(project())
+        implementation(libs.revoman)
+        implementation(libs.truth)
+        implementation(libs.mockito.core)
+        implementation(libs.spring.beans)
+        implementation(libs.json.assert)
+        implementation(libs.assertj.vavr)
+      }
+    }
+  }
+}
+
+node { nodeProjectDir = file("${project.projectDir}/js") }
+
+tasks {
+  check { dependsOn(npmInstall) }
+  test { dependsOn(npmInstall) }
+}
+
+kover { reports { total { html { onCheck = true } } } }
+
+moshi { enableSealed = true }
+
+nexusPublishing { this.repositories { sonatype { stagingProfileId = STAGING_PROFILE_ID } } }
---
+++ settings.gradle.kts
@@ -0,0 +1,36 @@
+/**
+ * ************************************************************************************************
+ * Copyright (c) 2023, Salesforce, Inc. All rights reserved. SPDX-License-Identifier: Apache License
+ * Version 2.0 For full license text, see the LICENSE file in the repo root or
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * ************************************************************************************************
+ */
+plugins { id("com.gradle.develocity") version "3.19.2" }
+
+dependencyResolutionManagement {
+  versionCatalogs { create("libs") { from(files("libs.versions.toml")) } }
+  pluginManagement {
+    repositories {
+      mavenCentral()
+      gradlePluginPortal()
+      google()
+      maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+      maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+  }
+}
+
+val isCI = !System.getenv("CI").isNullOrEmpty()
+
+develocity {
+  buildScan {
+    publishing.onlyIf {
+      it.buildResult.failures.isNotEmpty() && !System.getenv("CI").isNullOrEmpty()
+    }
+    uploadInBackground.set(!isCI)
+    termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+    termsOfUseAgree = "yes"
+  }
+}
+
+rootProject.name = "ape-root"
