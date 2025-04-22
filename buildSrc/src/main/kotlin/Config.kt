73b05f09443edfa58ac17a1500c624a598c333ce
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2025-04-22 17:36:47 +0530

    commit
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -5,7 +5,7 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  * ************************************************************************************************
  */
-const val GROUP_ID = "com.salesforce.revoman"
-const val VERSION = "0.7.8"
-const val ARTIFACT_ID = "revoman"
+const val GROUP_ID = "com.salesforce.ape"
+const val VERSION = "0.0.1"
+const val ARTIFACT_ID = "ape"
 const val STAGING_PROFILE_ID = "1ea0a23e61ba7d"
